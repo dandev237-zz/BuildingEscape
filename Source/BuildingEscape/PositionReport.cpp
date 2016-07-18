9e1573ffeb4297f008fe872bdc2ca5d29824e579
Daniel Castro García <dandev237@gmail.com>
2016-07-18 03:04:59 +0200

    Added OpenDoor script, layed out level geometry

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -22,7 +22,9 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *ObjectName);
+	FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPosition);
 	
 }
 
